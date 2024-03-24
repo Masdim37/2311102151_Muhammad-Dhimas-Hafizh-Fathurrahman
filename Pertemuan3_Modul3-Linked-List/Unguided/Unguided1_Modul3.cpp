1fb93bf79bcc517828e3e7146e6372454442cc27
Masdim37 <dhimas.hfzh375@gmail.com>
2024-03-24 23:37:51 +0700

    nyicil unguided 1

--- Pertemuan3_Modul3-Linked-List/Unguided/Unguided1_Modul3.cpp
+++ Pertemuan3_Modul3-Linked-List/Unguided/Unguided1_Modul3.cpp
@@ -2,6 +2,102 @@
 
 using namespace std;
 
+//Deklarasi Struct node
+struct node{ //node berisi variable nama_151, umur_151, dan pointer next
+    string nama_151;
+    int umur_151
+    node *next;
+};
+    node *head; //deklrasi node sebagai head
+    node *tail; //deklarasi node sebagai tail
+
+//prosedur inisialisasi head & tail
+void inisialisasi_151(){
+    //mengosongkan nilai head dan tail 
+    head = NULL;
+    tail = NULL;
+}
+
+//prosedur pengecekan head & tail
+void kosong_151(){
+    if(head == NULL){
+        return true; //jika head tidak ada isinya (NULL), maka true
+    } else {
+        return false; //jika head ada isinya, maka false
+    }
+}
+
+//prosedur tambah depan
+void TambahDepan_151(string name_151, int age_151){
+    node *baru = new node; //tambah node baru
+    baru->nama_151 = name_151; //isi node baru
+    baru->umur_151 = age_151; //isi node baru
+    baru->next = NULL; // next node nya NULL
+    //ini artinya node baru tersebut blom digabung ke linked list
+    if(kosong_151 == true){ //jika linked listnya masih kosong, maka node baru adalah head dan tailnya
+        head = tail = baru;
+        tail->next = NULL //tail dari node baru = NULL
+    } else {
+        baru->next = head; //jika linked listnya tidak kosong, maka next node baru dihubungkan ke head linked list
+        head = baru; //dan node baru dijadikan head
+    }
+}
+
+//prosedur tambah belakang
+void TambahBelakang_151(string name_151, int age_151){
+    node *baru = new node; //tambah node baru
+    baru->nama_151 = name_151; //isi node baru
+    baru->umur_151 = age_151; //isi node baru
+    baru->next = NULL; // next node nya NULL
+    //ini artinya node baru tersebut blom digabung ke linked list
+    if(kosong_151 == true){ //jika linked listnya masih kosong, maka node baru adalah head dan tailnya
+        head = tail = baru;
+        tail->next = NULL //tail dari node baru = NULL
+    } else {
+        tail->next = baru; //jika linked listnya tidak kosong, maka next node baru dihubungkan ke head linked list
+        tail = baru; //dan node baru dijadikan head
+    }
+}
+
+//fungsi hitung node
+int HitungNode_151(){
+    node *hitung; //tambah node hitung
+    hitung = head; //node hitung sebagai head
+    int jumlah = 0;
+    while(hitung != NULL){ //perulangan while
+        jumlah++; //deklarasi variabel jumlah
+        hitung = hitung->next; //jika next node hitung bukan NULL, maka variabel jumlah +1
+    }
+    return jumlah;
+}
+
+//tambah tengah
+void TambahTengah_151(string name_151, int age_151, int posisi_151){
+    if(posisi_151 < 1 || posisi_151 > HitungNode_151){ //jika posisi node yang akan ditambahkan kurang dari 1 atau lebih dari jumlah node yang ada, maka tampilkan output
+        cout << "Posisi node yang akan ditambahkan offside" << endl;
+    } else if(posisi_151 == 1){ //jika posisi node yang akan ditambahkan adalah posisi pertama, maka tampilkan output
+        cout << "Posisi node yang akan ditambahkan bukan di tengah" << endl;
+    } else {
+        //tambah node baru & node bantu
+        node *baru, *bantu;
+        *baru = new node; //buat node baru
+        baru->nama_151 = name_151; //isi node baru
+        baru->umur_151 = age_151; //isi node baru
+        baru->next = NULL; //next nodenya NULL
+        //transvering
+        bantu = head; //node bantu dijadikan head
+        int nomor_151 = 1; //deklarasi variabel nomor_151
+        //hitung posisi node baru akan ditambahkan sebagai node keberapa
+        while(nomor_151 < posisi_151 - 1){ //ketika variabel nomor_151 kurang dari posisi node baru yang akan ditambahkan - 1, maka
+            bantu = bantu->next; //node bantu berlanjut ke node selanjutnya sampai variabel nomor = posisi node baru yang akan ditambahkan
+            nomor_151++; //variable nomor_151 +1
+        }
+        //maka akan diketahui posisi node baru yang akan ditambahkan berada dimana
+        baru->next = bantu->next; // next node baru sama dengan next node bantu
+        bantu->next = baru; //next node bantu sama dengan node baru 
+    }
+}
+
 int main(){
 
     
